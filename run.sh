7cb64671930b24f00467349f21cd4bfcb8123add
asegura4488 <asegura4488@gmail.com>
2018-07-16 15:11:54 -0500

    submmit OK

--- run.sh
+++ run.sh
@@ -1,53 +1,105 @@
-#DATA
-DATAFILECFG[0]="SAMPLES_LIST_DATA_MET.cfg"
-DATAFILECFG[1]="SAMPLES_LIST_DATA_SingleMuon.cfg"
-DATAFILECFG[2]="SAMPLES_LIST_DATA_TAU.cfg"
+#!/bin/bash
+#############################################################
+####         DATA
+#############################################################
+##MET
+DATA_CFG[0]="SAMPLES_LIST_DATA_MET"
+DATA_CFG[1]="SAMPLES_LIST_DATA_TAU"
+DATA_CFG[2]="SAMPLES_LIST_DATA_SingleMuon"
+##TAU
+DATA_FOLDER[0]="MET"
+DATA_FOLDER[1]="TAU"
+DATA_FOLDER[2]="SingleMuon"
+#SINGLE
+DATA_SINGLE_CFG[0]="SAMPLES_LIST_DATA_SingleMuonB"
+DATA_SINGLE_CFG[1]="SAMPLES_LIST_DATA_SingleMuonC"
+DATA_SINGLE_CFG[2]="SAMPLES_LIST_DATA_SingleMuonD"
+DATA_SINGLE_CFG[3]="SAMPLES_LIST_DATA_SingleMuonE"
+DATA_SINGLE_CFG[4]="SAMPLES_LIST_DATA_SingleMuonF"
+DATA_SINGLE_CFG[5]="SAMPLES_LIST_DATA_SingleMuonG"
+DATA_SINGLE_CFG[6]="SAMPLES_LIST_DATA_SingleMuonH"
 #########################################
-DATAFOLDER[0]="MET"
-DATAFOLDER[1]="SingleMuon"
-DATAFOLDER[2]="TAU"
-#MC
-MCFILECFG[0]="SAMPLES_LIST_Bkg_with_QCD.cfg"
-#MCFILECFG[0]="SAMPLES_LIST_MC_All.cfg"
-MCFILECFG[1]="SAMPLES_LIST_MC_DYJets.cfg"
-MCFILECFG[2]="SAMPLES_LIST_MC_TT.cfg"
-MCFILECFG[3]="SAMPLES_LIST_MC_VV.cfg"
-MCFILECFG[4]="SAMPLES_LIST_MC_WJets.cfg"
+DATA_SINGLE_FOLDER[0]="SingleMuonB"
+DATA_SINGLE_FOLDER[1]="SingleMuonC"
+DATA_SINGLE_FOLDER[2]="SingleMuonD"
+DATA_SINGLE_FOLDER[3]="SingleMuonE"
+DATA_SINGLE_FOLDER[4]="SingleMuonF"
+DATA_SINGLE_FOLDER[5]="SingleMuonG"
+DATA_SINGLE_FOLDER[6]="SingleMuonH"
+############################################################
+####          MC
+###########################################################
+MC_CFG[0]="SAMPLES_LIST_MC_All"
+MC_CFG[1]="SAMPLES_LIST_MC_All_QCD"
+MC_CFG[2]="SAMPLES_LIST_MC_DYJets"
+MC_CFG[3]="SAMPLES_LIST_MC_TT"
+MC_CFG[4]="SAMPLES_LIST_MC_ST"
+MC_CFG[5]="SAMPLES_LIST_MC_VV"
+MC_CFG[6]="SAMPLES_LIST_MC_WJets"
 #########################################
-MCFOLDER[0]="AllMC"
-MCFOLDER[1]="DYJets"
-MCFOLDER[2]="TT"
-MCFOLDER[3]="VV"
-MCFOLDER[4]="WJets"
+MC_FOLDER[0]="AllMC"
+MC_FOLDER[1]="AllMC_QCD"
+MC_FOLDER[2]="DYJets"
+MC_FOLDER[3]="TT"
+MC_FOLDER[4]="ST"
+MC_FOLDER[5]="VV"
+MC_FOLDER[6]="WJets"
+############################################################
+#### 	    SIGNAL
+###########################################################
+SIGNAL_CFG[0]="SAMPLES_LIST_SIGNAL_STAU"
+SIGNAL_FOLDER[0]="SIGNAL_STAU"
 
-###
+
+###################### Parameters
 EXE="remote.py"
+FINAL="RESULT"
 
-echo "Please enter Data (0) or MC (1)"
+echo "Please enter Data (0), MC (1) or Signal (2)"
 read type_d
 
 if [ "$type_d" -eq 0 ]; then
 echo Please Choose the sample: 
-for j in `seq 0 2` ;
+for i in `seq 0 2` ;
 do
-echo $j for: ${DATAFOLDER[$j]}
+echo $i for: ${DATA_FOLDER[$i]}
+done
+
+echo ------------------------
+read idata
+
+	if [ "$idata" -eq 2 ]; then
+	for j in `seq 0 6` ;
+	do
+	echo Running ${DATA_SINGLE_CFG[$j]} to $FINAL/${DATA_SINGLE_FOLDER[$j]}
+	./$EXE ${DATA_SINGLE_CFG[$j]}.cfg -t $FINAL/${DATA_SINGLE_FOLDER[$j]} --force
+	done
+
+	else
+	echo Running ${DATA_CFG[$idata]} to $FINAL/${DATA_FOLDER[$idata]}
+	./$EXE ${DATA_CFG[$idata]}.cfg -t $FINAL/${DATA_FOLDER[$idata]} --force
+	fi	
+
+
+elif [ "$type_d" -eq 1 ]; then
+echo Please Choose the MC sample: 
+for j in `seq 0 6` ;
+do
+echo $j for: ${MC_FOLDER[$j]}
 done
 echo ------------------------
-read index
-echo running ----- ${DATAFILECFG[$index]} ----- to folder ----- ${DATAFOLDER[$index]}
-./$EXE ${DATAFILECFG[$index]} -t ${DATAFOLDER[$index]} --force
+read imc
+echo Running ${MC_CFG[$imc]} to $FINAL/${MC_FOLDER[$imc]}
+./$EXE ${MC_CFG[$imc]}.cfg -t $FINAL/${MC_FOLDER[$imc]} --force
+
+        	
+
+elif [ "$type_d" -eq 2 ]; then        
+echo Running ${SIGNAL_CFG[0]} to $FINAL/${SIGNAL_FOLDER[0]}
+./$EXE ${SIGNAL_CFG[0]}.cfg -t $FINAL/${SIGNAL_FOLDER[0]} --force
+
 else
-	if [ "$type_d" -eq 1 ]; then
-	echo Please Choose the sample: 
-	for j in `seq 0 4` ;
-	do
-	echo $j for: ${MCFOLDER[$j]}
-	done
-	echo ------------------------
-	read index
-	echo running ----- ${MCFILECFG[$index]} ----- to folder ----- ${MCFOLDER[$index]}
-	./$EXE ${MCFILECFG[$index]} -t ${MCFOLDER[$index]} --force 
-	else
-	echo no valid option
-	fi
+echo No valid option
+			
+		
 fi
